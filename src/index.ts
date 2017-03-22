2dfca1a865e622c6bde546ab14954199a779d724
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-03-22 21:06:38 +0100

    Configure the simple-crawler package.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-function crawl() {
-    
-}
+import * as Crawler from 'simplecrawler';
+
+console.log('Initializing crawler...');
+
+let crawler = Crawler("http://www.example.com/")
+    .on("fetchcomplete", () => {
+        console.log("Fetched a resource!")
+    });
+
+crawler.start();
