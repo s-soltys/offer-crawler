1096555f8a289ee5e16888790eed1d82a9b310da
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-03-22 21:34:52 +0100

    Implement the 'crawler' package.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import * as Crawler from 'simplecrawler';
+import * as Crawler from 'crawler';
+import * as http from 'http';
 
 console.log('Initializing crawler...');
 
-let crawler = Crawler("http://www.example.com/")
-    .on("fetchcomplete", () => {
-        console.log("Fetched a resource!")
-    });
+let crawler = new Crawler({
+    maxConnections : 10,
+    callback: (error: Error, res: http.IncomingMessage, done: Function) => {
+        if (error) {
+            console.log(error);
+        } else {
+            let options = (res as any).options;
+            let $: JQueryStatic = (res as any).$;
+            
+            console.log(`
+            *** Crawled to ${options.uri}
+                Page title: ${$("title").text()}
+                Link on page: ${$("a").attr('href')}
+            `);
+        }
+        done();
+    }
+});
 
-crawler.start();
+crawler.queue(['http://www.example.com', 'http://www.example.com?query=0']);
