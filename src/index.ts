9eb95fed2639567783ed664bafe9b8e59ad38f01
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-03-23 09:18:15 +0100

    Minor refactoring.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,4 @@
-import * as Crawler from 'crawler';
-import * as http from 'http';
+import { crawler } from './crawler';
 
 console.log('Initializing crawler...');
-
-let crawler = new Crawler({
-    maxConnections : 10,
-    callback: (error: Error, res: http.IncomingMessage, done: Function) => {
-        if (error) {
-            console.log(error);
-        } else {
-            let options = (res as any).options;
-            let $: JQueryStatic = (res as any).$;
-            
-            console.log(`
-            *** Crawled to ${options.uri}
-                Page title: ${$("title").text()}
-                Link on page: ${$("a").attr('href')}
-            `);
-        }
-        done();
-    }
-});
-
 crawler.queue(['http://www.example.com', 'http://www.example.com?query=0']);
